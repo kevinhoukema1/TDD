e76f2815f3be37a2b7a79de035d174d3386f95b9
kevinhoukema1 <kevin-houkema@hotmail.com>
2021-10-17 21:07:27 +0200

    Finished assignment 1

--- TDD/src/main/java/nl/hanze/hive/Player.java
+++ TDD/src/main/java/nl/hanze/hive/Player.java
@@ -1,19 +1,31 @@
 package nl.hanze.hive;
 
+import java.util.HashMap;
+
 public class Player {
     Hive.Player colour;
-    
-    public Player(){
+    HashMap<Hive.Tile, Integer> tiles;
 
-    }
-
-    public void setColour(Hive.Player colour){
+    public Player(Hive.Player colour){
+        this.tiles = new HashMap<Hive.Tile, Integer>();
         this.colour = colour;
-        
+        fillTiles();
     }
-    
+
     public Hive.Player getColour(){
         return this.colour;
 
+    }
+
+    public void fillTiles(){
+        this.tiles.put(Hive.Tile.QUEEN_BEE, 1);
+        this.tiles.put(Hive.Tile.SPIDER, 2);
+        this.tiles.put(Hive.Tile.BEETLE, 2);
+        this.tiles.put(Hive.Tile.SOLDIER_ANT, 3);
+        this.tiles.put(Hive.Tile.GRASSHOPPER, 3);
+    }
+
+    public HashMap<Hive.Tile, Integer> getTiles(){
+        return this.tiles;
     }
 }
--- TDD/src/test/java/nl/hanze/hive/SpelmateriaalSpec.java
+++ TDD/src/test/java/nl/hanze/hive/SpelmateriaalSpec.java
@@ -3,6 +3,8 @@ package nl.hanze.hive;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashMap;
+
 import javax.naming.TimeLimitExceededException;
 
 import org.junit.jupiter.api.Test;
@@ -14,30 +16,146 @@ import org.junit.jupiter.api.Test;
 public class SpelmateriaalSpec {
     
     @Test 
-    void testIfPlayerTileColourIsWhite(){
+    void testIfPlayerWhiteColourIsWhite(){
 
         //arrange
-        Player whiteTile = new Player();
+        Player whiteTile = new Player(Hive.Player.WHITE);
 
-        //act
-        whiteTile.setColour(Hive.Player.WHITE);
-        
         //assert
         assertEquals(Hive.Player.WHITE, whiteTile.getColour());
 
     }
     
     @Test 
-    void testIfPlayerTileColourIsBlack(){
+    void testIfPlayerBlackColourIsBlack(){
 
         //arrange
-        Player blackTile = new Player();
+        Player blackTile = new Player(Hive.Player.BLACK);
 
-        //act
-        blackTile.setColour(Hive.Player.BLACK);
         
         //assert
         assertEquals(Hive.Player.BLACK, blackTile.getColour());
 
     } 
+
+    @Test
+    void testIfTileIsBeeQeueen(){
+        
+        //Arragne
+        Tile tile = new Tile(Hive.Tile.QUEEN_BEE);
+
+        //Assert
+        assertEquals(Hive.Tile.QUEEN_BEE, tile.getType());
+    } 
+
+    @Test
+    void testIfTileIsSpoder(){
+        
+        //Arragne
+        Tile tile = new Tile(Hive.Tile.SPIDER);
+
+        //Assert
+        assertEquals(Hive.Tile.SPIDER, tile.getType());
+    } 
+
+    @Test
+    void testIfTileIsBeetle(){
+        
+        //Arragne
+        Tile tile = new Tile(Hive.Tile.BEETLE);
+
+        //Assert
+        assertEquals(Hive.Tile.BEETLE, tile.getType());
+    } 
+
+    @Test
+    void testIfTileIsSS(){
+        
+        //Arragne
+        Tile tile = new Tile(Hive.Tile.SOLDIER_ANT);
+
+        //Assert
+        assertEquals(Hive.Tile.SOLDIER_ANT, tile.getType());
+    } 
+
+    @Test
+    void testIfTileIsGrassHoper(){
+        
+        //Arragne
+        Tile tile = new Tile(Hive.Tile.GRASSHOPPER);
+
+        //Assert
+        assertEquals(Hive.Tile.GRASSHOPPER, tile.getType());
+    } 
+
+
+    // <QUEENBEE,   1>
+    // <SPODER,     2>
+    // <BEETLE,     2>
+    // <SOLDIERANT, 3>
+    // <GRASSHOPPER,3>
+
+    @Test
+    void testIfPlayerGetsCorrectAmmountOfQueenBee(){
+        //Arrange
+        Player p1 = new Player(Hive.Player.WHITE);
+
+        //Act
+        HashMap<Hive.Tile, Integer> p1Tiles =  p1.getTiles();
+        
+        //Assert
+        assertEquals(1, p1Tiles.get(Hive.Tile.QUEEN_BEE));
+    }
+
+    @Test
+    void testIfPlayerGetsCorrectAmmountOfSpoders(){
+        //Arrange
+        Player p1 = new Player(Hive.Player.WHITE);
+
+        //Act
+        HashMap<Hive.Tile, Integer> p1Tiles =  p1.getTiles();
+        
+        //Assert
+        assertEquals(2, p1Tiles.get(Hive.Tile.SPIDER));
+    }
+
+    @Test
+    void testIfPlayerGetsCorrectAmmountOfBeetle(){
+        //Arrange
+        Player p1 = new Player(Hive.Player.WHITE);
+
+        //Act
+        HashMap<Hive.Tile, Integer> p1Tiles =  p1.getTiles();
+        
+        //Assert
+        assertEquals(2, p1Tiles.get(Hive.Tile.BEETLE));
+    }
+
+    @Test
+    void testIfPlayerGetsCorrectAmmountOfSS(){
+        //Arrange
+        Player p1 = new Player(Hive.Player.WHITE);
+
+        //Act
+        HashMap<Hive.Tile, Integer> p1Tiles =  p1.getTiles();
+        
+        //Assert
+        assertEquals(3, p1Tiles.get(Hive.Tile.SOLDIER_ANT));
+    }
+
+    @Test
+    void testIfPlayerGetsCorrectAmmountOfGrasshopper(){
+        //Arrange
+        Player p1 = new Player(Hive.Player.WHITE);
+
+        //Act
+        HashMap<Hive.Tile, Integer> p1Tiles =  p1.getTiles();
+        
+        //Assert
+        assertEquals(3, p1Tiles.get(Hive.Tile.GRASSHOPPER));
+    }
+
+
+     
+    
 }
