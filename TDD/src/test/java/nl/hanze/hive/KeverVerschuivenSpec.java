39cc7802d540bfff0a827f864f665415bc5e4e86
kevinhoukema1 <kevin-houkema@hotmail.com>
2021-11-17 12:14:39 +0100

    Test 7 failes

---
+++ TDD/src/test/java/nl/hanze/hive/KeverVerschuivenSpec.java
@@ -0,0 +1,56 @@
+package nl.hanze.hive;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.naming.TimeLimitExceededException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import nl.hanze.hive.Hive.IllegalMove;
+
+public class KeverVerschuivenSpec {
+    @Test // 7A
+    void testIfKeverMovesAtLeastOneTile() throws IllegalMove{
+        //Arrange
+        HiveGame game = new HiveGame();
+        Player p1 = new Player(Hive.Player.WHITE);
+        Player p2 = new Player(Hive.Player.BLACK);
+
+        //act
+        p1.playTile(game, Hive.Tile.QUEEN_BEE, 0, 0);
+        p2.playTile(game, Hive.Tile.QUEEN_BEE, -1, 0);
+
+        p1.playTile(game, Hive.Tile.BEETLE, 1, 0);
+        p2.pass(game);
+
+        p1.moveTile(game, 1, 0, 1, -1);
+        
+        //assert
+        assertEquals(Hive.Tile.BEETLE, game.getBoard().getTilePosition(new Coordinate(1, -1)).getType());
+
+        
+    }
+
+    @Test // 7A.2
+    void testIfErrorIfBeetleMovesMoreThanOne() throws IllegalMove{
+       //Arrange
+        HiveGame game = new HiveGame();
+        Player p1 = new Player(Hive.Player.WHITE);
+        Player p2 = new Player(Hive.Player.BLACK);
+
+        //act
+        p1.playTile(game, Hive.Tile.QUEEN_BEE, 0, 0);
+        p2.playTile(game, Hive.Tile.QUEEN_BEE, -1, 0);
+
+        p1.playTile(game, Hive.Tile.BEETLE, 1, 0);
+        p2.pass(game); 
+
+        assertThrows(Hive.IllegalMove.class, ()-> { p1.moveTile(game, 1, 0, 0, -1); });
+    }
+}
