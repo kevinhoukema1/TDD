688dccfa541790aa099bb10ff1828df43042f4e8
kevinhoukema1 <kevin-houkema@hotmail.com>
2021-11-17 16:07:32 +0100

    10A fails

---
+++ TDD/src/test/java/nl/hanze/hive/SpoderVerschuivenSpec.java
@@ -0,0 +1,38 @@
+package nl.hanze.hive;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.naming.TimeLimitExceededException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import nl.hanze.hive.Hive.IllegalMove;
+
+
+public class SpoderVerschuivenSpec {
+    @Test //10a
+    void testIfSpoderMovesAtPreciselyThreeTiles() throws IllegalMove{
+        //arrange
+        HiveGame game = new HiveGame();
+        Player p1 = new Player(Hive.Player.WHITE);
+        Player p2 = new Player(Hive.Player.BLACK);
+
+        //act
+        p1.playTile(game, Hive.Tile.QUEEN_BEE, 0, 0);
+        p2.playTile(game, Hive.Tile.QUEEN_BEE, -1, 0);
+
+        p1.playTile(game, Hive.Tile.SPIDER, 1, 0);
+        p2.pass(game);
+
+        p1.moveTile(game, 1, 0, -2, 1);
+
+        assertEquals(Hive.Tile.SPIDER,  game.getBoard().getTilePosition(new Coordinate(-2, 1)).getType());
+    }
+}
