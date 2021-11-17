c591781bb37cb463b7ed8b720d4f4a8c5beb06fc
kevinhoukema1 <kevin-houkema@hotmail.com>
2021-11-17 20:03:28 +0100

    11a failes

--- TDD/src/test/java/nl/hanze/hive/GrasshopperVerschuivenSpec.java
+++ TDD/src/test/java/nl/hanze/hive/GrasshopperVerschuivenSpec.java
@@ -16,5 +16,22 @@ import org.junit.jupiter.api.Test;
 import nl.hanze.hive.Hive.IllegalMove;
 
 public class GrasshopperVerschuivenSpec {
-    
+    @Test //11a
+    void testIfGrassHopperCanJumpOnAStraightLine() throws IllegalMove{
+        //arrange
+        HiveGame game = new HiveGame();
+        Player p1 = new Player(Hive.Player.WHITE);
+        Player p2 = new Player(Hive.Player.BLACK);
+
+        //act
+        p1.playTile(game, Hive.Tile.QUEEN_BEE, 0, 0);
+        p2.playTile(game, Hive.Tile.QUEEN_BEE, -1, 0);
+
+        p1.playTile(game, Hive.Tile.GRASSHOPPER, 1, 0);
+        p2.playTile(game, Hive.Tile.GRASSHOPPER, -1, -1);
+
+        p1.moveTile(game, 1, 0, -2, 0);
+
+        assertEquals(Hive.Tile.GRASSHOPPER, game.getBoard().getTilePosition(new Coordinate(-2, 0)).getType());
+    }
 }
